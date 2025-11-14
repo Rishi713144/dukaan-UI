9d652520bfb440437fa36517bc83838460e5912d
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-11-14 15:31:26 +0530

    initial commit

---
+++ UI/src/components/Fotter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fotter() {
+    return (
+        <div className='flex justify-center items-center flex-col text-black-300 mb-6 gap-2'>
+            --© Rishi--
+            </div>
+    )
+}
+
+export default Fotter
